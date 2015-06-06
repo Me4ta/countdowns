1bb5436be9a6f145064c04224ee16a0fcf6a20a0
Helen Akimova <me4ta18@gmail.com>
2015-06-05 18:32:23 -0700

    Revert "Changed setTiomout function"
    This reverts commit 7d2f7e223d9fa147ef98b30d36e467b59d7e7a0a.

--- countdown_timer.js
+++ countdown_timer.js
@@ -1,6 +1,6 @@
 var secondsNumber = 10;
-setTimeout(function () {
-    for (i = secondsNumber; i >= 0; i --) {
+for (i = secondsNumber; i >= 0; i --) {
+    setTimeout(function (i) {
         document.write("Hey there, " + i + " seconds left till changing backgroud colour ");
-    }
-}, 1000);
+    }, 1000);
+}
