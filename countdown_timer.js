1053efdd43591109022a3c2a5cfce36f508e6b4b
Helen Akimova <me4ta18@gmail.com>
2015-06-05 19:44:48 -0700

    Add clearInterval function

--- countdown_timer.js
+++ countdown_timer.js
@@ -1,7 +1,11 @@
 var secondsNumber = 3;
 var i = secondsNumber;
-setInterval(function () {
+var timeFunction = setInterval(function () {
     document.body.innerHTML = ("Hey there, " + i + " seconds left till changing backgroud colour ");
     i--;
+    if (i == -1) {
+        clearInterval(timeFunction);
+    } 
 }, 1000);
+
     
