ddf3d74b9ad165d50a76235284c61720a6473822
Helen Akimova <me4ta18@gmail.com>
2015-06-05 18:49:03 -0700

    Revert "Add function parameter"
    This reverts commit d236be57f2792eaecad8ebbb0ce9005ebc8b1d67.

--- countdown_timer.js
+++ countdown_timer.js
@@ -1,6 +1,6 @@
 var secondsNumber = 10;
 for (i = secondsNumber; i >= 0; i --) {
-    setTimeout(function (i) {
+    setTimeout(function () {
         document.write("Hey there, " + i + " seconds left till changing backgroud colour ");
     }, 1000);
 }
