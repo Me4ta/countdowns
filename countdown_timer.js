fe528e0e41f676dceaa49cc182d8667f994dc042
Helen Akimova <me4ta18@gmail.com>
2015-06-10 22:15:54 -0700

    Add changing colors

--- countdown_timer.js
+++ countdown_timer.js
@@ -1,10 +1,14 @@
 var secondsNumber = 3;
 var i = secondsNumber;
 var timeFunction = setInterval(function () {
-    document.body.innerHTML = ("Hey there, " + i + " seconds left till changing backgroud colour ");
+    //document.body.innerText = ("Hey there, " + i + " seconds left till changing backgroud colour ");
+    var x = document.querySelectorAll("p");
+    x[0].innerText = 'Hey there, ' + i + ' seconds left till changing backgroud colour';
     i--;
     if (i == -1) {
         clearInterval(timeFunction);
+        //change bgcolor here
+        x[0].className = 'new-color';
     } 
 }, 1000);
 
