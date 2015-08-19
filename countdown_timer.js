c20635b43e329b9eaa546514b99da9ac7fcf9b89
Helen Akimova <me4ta18@gmail.com>
2015-08-18 22:27:46 -0700

    added function, which gets date from input window

--- countdown_timer.js
+++ countdown_timer.js
@@ -1,5 +1,5 @@
 ////create moment object for some date
-var someDate = moment('08-19-2015 18.00', 'MM-DD-YYYY HH.mm');
+var someDate = moment('08-19-2015 21.45', 'MM-DD-YYYY HH.mm');
 
 var now = moment();
 var durationFromNowToSome = moment.duration({from: now, to: someDate});
@@ -16,22 +16,24 @@ function getFullTime(duration) {
             duration.get('seconds') + ' seconds';
 }
 
+function getSomeDate() {
+    var x = document.getElementById("someDate").value;
+    someDate = x;
+}
+
 //wait for DOM to get loaded
-document.addEventListener('DOMContentLoaded', function(event) { 
+document.addEventListener('DOMContentLoaded', function() { 
     var pElements = document.querySelectorAll('p');
     //var timeElement1 = pElements[0];
     var timeElement2 = pElements[1];
     //var timeElement3 = pElements[2];
     //var timeElement4 = pElements[3];
 
-
     var timeFunction = setInterval(function () {
         
         //timeElement1.innerText = 'I\'m subtracting seconds and ' + getFullTime(durationFromNowToSome) + ' left';
-        
-
-        currentTime = moment();
-        durationFromCurrentToSome = moment.duration({from: currentTime, to: someDate});
+        var currentTime = moment();
+        var durationFromCurrentToSome = moment.duration({from: currentTime, to: someDate});
 
         timeElement2.innerText = 'I\'m counting difference and ' + getFullTime(durationFromCurrentToSome) + ' left';
 
@@ -48,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
             clearInterval(timeFunction);
             //change bgcolor here
             timeElement2.className = 'new-color';
-            timeElement2.innerText = ('Today is ' + moment().format('MM-DD-YYYY hh:mm:ss a'));
+            //timeElement2.innerText = ('Today is ' + moment().format('MM-DD-YYYY hh:mm:ss a'));
         } 
 
     }, 1000);
